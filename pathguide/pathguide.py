5471b76fc00f55dcc08a338703af36be5d23c5b4
Oscar Katz <ogkatz@googlemail.com>
2023-12-23 16:39:46 +0000

    pathguide

---
+++ pathguide/pathguide.py
@@ -0,0 +1,170 @@
+from __future__ import annotations
+
+from pydantic import BaseModel
+import typing as t
+import itertools as it
+from functools import cached_property
+
+from tomli import loads as tloads
+
+from dataclasses import dataclass
+
+class RulesObj(BaseModel):
+    name: str
+    full_name: str
+    text: t.Optional[str] = None
+    replacements: t.Dict[str, t.Dict] = {}
+    article_redirect: t.Optional[str] = None
+    url_prefix: t.ClassVar[t.Optional[str]] = None
+
+    def __hash__(self):
+        return hash(self.name)
+    
+    def get_article(self) -> str:
+        try:
+            return self.url_prefix + self.article_redirect if self.article_redirect else self.url_prefix + self.name
+        
+        except:
+            raise NotImplementedError
+
+
+def parse_feat_elem(feat_elems: t.List[str | t.List[str]]):
+    for feat_elem in feat_elems:
+        if isinstance(feat_elem, str):
+            ret = feat_dict.get(feat_elem, NonDBFeat(name=feat_elem))
+
+            if isinstance(ret, NonDBFeat):
+                yield ret
+            
+            else:
+                if ret.subfeats:
+                    yield from parse_feat_elem(ret.subfeats)
+                else:
+                    yield ret
+
+        else:
+            yield FeatUnion(name = feat_elem[0], elements = [obj for obj in parse_feat_elem(feat_elem[1:])])
+
+class Required(BaseModel):
+    feats: t.List[str | t.List[str]] = []
+
+    @cached_property
+    def feat_objs(self) -> t.List[Feat | FeatUnion | NonDBFeat]:
+        return {obj for obj in parse_feat_elem(self.feats)}
+    
+    def __add__(self, other):
+        if type(other) != type(self):
+            return NotImplemented
+        else:
+            return type(self)(**{k: getattr(self, k) + getattr(other, k) for k in self.model_fields.keys()})
+
+class Style(RulesObj):
+    upsides: t.List[str] = []
+    downsides: t.List[str] = []
+    required: t.Optional[Required] = None
+    url_prefix: t.ClassVar[t.Optional[str]] = "/styles/"
+    recommended: t.Optional[Required] = None
+
+    def __repr__(self):
+        return "Style(" + ", ".join((self.name, str(self.replacements), str(self.required))) + ")"
+
+class Replacement(BaseModel):
+    condition: str
+    replace_dict: t.Dict[str, t.Any]
+
+class Class(Style):
+    provides: t.Optional[Required] = None
+    url_prefix: t.ClassVar[t.Optional[str]] = "/classes/"
+
+    def __repr__(self):
+        return "Class(" + ", ".join((self.name, str(self.replacements), str(self.required))) + ")"
+
+class Feat(RulesObj):
+    subfeats: t.Optional[t.List[str]] = None
+    prereqs: t.Optional[Required] = None
+    url_prefix: t.ClassVar[t.Optional[str]] = "/feats/"
+    is_union: t.ClassVar[bool] = False
+    is_db: t.ClassVar[bool] = True
+
+    @property
+    def title(self):
+        if self.subfeats:
+            return self.full_name + " Line"
+    
+    @property
+    def full_line(self):
+        if self.subfeats:
+            return " -> ".join((feat.title() for feat in self.subfeats))
+        else:
+            return self.full_name
+
+    def is_provided(self, provideddict: t.Dict[str, t.Set[str]]):
+        return self.name in provideddict.feats
+
+class NonDBFeat(BaseModel):
+    name: str
+    is_db: t.ClassVar[bool] = False
+
+    def get_article(self):
+        return ""
+    
+    @cached_property
+    def full_name(self):
+        return self.name.title().replace("_", " ")
+    
+    def __hash__(self):
+        return hash(self.name)
+    
+    def is_provided(self, provideddict: t.Dict[str, t.Set[str]]):
+        return self.name in provideddict.feats
+
+
+class FeatUnion(BaseModel):
+    name: str
+    elements: t.List[Feat | NonDBFeat]
+    is_union: t.ClassVar[bool] = True
+    
+    def __iter__(self):
+        return iter(self.elements)
+    
+    def __hash__(self):
+        return hash(self.name)
+    
+    def is_provided(self, provideddict: t.Dict[str, t.Set[str]]):
+        return self.name in provideddict.feats
+
+@dataclass
+class OmniMap:
+    styles: t.Dict[str, Style]
+    classes: t.Dict[str, Class]
+    feats: t.Dict[str, Feat]
+
+# def amend_toml_subelems(style: str | dict):
+#     if not isinstance(style, dict):
+#         return style
+    
+#     return {k: amend_toml_subelems(v) for k, v in style.items()}
+
+def parse_toml_elem(base_name: str, style: dict, cls) -> RulesObj:
+    if "full_name" not in style:
+        style["full_name"] = base_name.title().replace("_", " ")
+
+    style["name"] = base_name
+
+    ret = cls.model_validate(style)
+    
+    return ret
+
+with open("feats.toml", "r") as f:
+    toml = tloads(f.read())
+    feat_dict = {base_name: parse_toml_elem(base_name, feat, Feat) for base_name, feat in toml.items()}
+
+with open("styles.toml", "r") as f:
+    toml = tloads(f.read())
+    style_dict = {base_name: parse_toml_elem(base_name, style, Style) for base_name, style in toml.items()}
+
+with open("classes.toml", "r") as f:
+    toml = tloads(f.read())
+    class_dict = {base_name: parse_toml_elem(base_name, cls, Class) for base_name, cls in toml.items()}
+
+omnimap = OmniMap(styles=style_dict, classes = class_dict, feats = feat_dict)
---
+++ pathguide/webpage.py
@@ -0,0 +1,174 @@
+from fastapi import FastAPI, Query, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from hypercorn.asyncio import serve
+import asyncio
+from hypercorn.config import Config
+import logging
+import typing as t
+from pathguide import omnimap, Required
+from itertools import zip_longest, chain
+
+config = Config()
+config.bind = ['0.0.0.0:8000']
+config.access_log_format = '%(R)s %(s)s %(st)s %(D)s %({Header}o)s'
+config.accesslog = logging.getLogger(__name__)
+config.loglevel = 'INFO'
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+def parse_cookie_adj(cookie_adj: str, cookies: t.Dict[str, str]):
+    # cookie_adj = e.g. rm.styles.twf
+    op, key, obj = cookie_adj.split(".")
+    cookie_str = cookies.get(key, "")
+
+    match op:
+        case "rm":
+            idx = cookies[key].find(obj)
+            length = len(obj) + 1
+            
+            val = cookie_str[:idx] + cookie_str[idx + length:]
+        
+        case "add":
+            val = cookie_str + obj + " "
+
+        case _:
+            raise ValueError("Unrecognised cookie command")
+    
+    return key, val
+
+def gen_sidebar(cookies: t.Dict[str, str]) -> t.Dict[str, t.List[str]]:
+    ret = {k: v.split() for k, v in cookies.items()}
+    print("gen_s", ret)
+    return ret
+
+page_map = {
+    "feats": "feat.html",
+    "styles": "style.html",
+    "classes": "class.html"
+}
+
+def get_included_excluded(cookies: t.Dict[str, str], key: str):
+    included = []
+    not_included = []
+
+    cookie = cookies.get(key, [])
+
+    for style in getattr(omnimap, key).values():
+        if style.name in cookie:
+            included.append(style)
+        else:
+            not_included.append(style)
+    
+    return included, not_included
+
+def get_provides(classes):
+    if not classes:
+        return Required()
+    
+    iterclasses = iter(cls.provides for cls in classes)
+    cls = next(iterclasses)
+    
+    for itercls in iterclasses:
+        cls = cls + itercls
+
+    return cls
+        
+
+@app.get("/select/{page_name}", response_class=HTMLResponse)
+async def select(request: Request, page_name: str, cookie_adj: t.Optional[str] = None):
+    print("wow")
+    if cookie_adj:
+        cookiekey, cookieval = parse_cookie_adj(cookie_adj, request.cookies)
+        request.cookies[cookiekey] = cookieval
+
+    included_styles, not_included_styles = get_included_excluded(request.cookies, "styles")
+    included_classes, _ = get_included_excluded(request.cookies, "classes")
+
+    provides = get_provides(included_classes)
+
+    to_render = list(getattr(omnimap, page_name).values())
+
+    resp = templates.TemplateResponse("select.html",
+                                      {"request": request,
+                                       "cookies": gen_sidebar(request.cookies),
+                                       "omnimap": omnimap,
+                                       "included_styles": included_styles,
+                                       "not_included_styles": not_included_styles,
+                                       "included_classes": included_classes,
+                                       "name": page_name,
+                                       "to_render": to_render, 
+                                       "provides": provides})
+    
+    if cookie_adj:
+        print(cookiekey, cookieval)
+        resp.set_cookie(key = cookiekey, value = cookieval)
+
+    return resp
+
+@app.get("/{page_type}/{page_name}", response_class=HTMLResponse)
+async def read_item(request: Request, page_type: str, page_name: str, cookie_adj: t.Optional[str] = None):
+    bod = getattr(omnimap, page_type)[page_name]
+
+    if cookie_adj:
+        cookiekey, cookieval = parse_cookie_adj(cookie_adj, request.cookies)
+        request.cookies[cookiekey] = cookieval
+    
+    included_styles, not_included_styles = get_included_excluded(request.cookies, "styles")
+    included_classes, _ = get_included_excluded(request.cookies, "classes")
+
+    provides = get_provides(included_classes)
+    
+    resp = templates.TemplateResponse(page_map[page_type], 
+                                      {"request": request, 
+                                       "cookies": gen_sidebar(request.cookies), 
+                                       "body": bod, 
+                                       "included_styles": included_styles,
+                                       "included_classes": included_classes,
+                                       "not_included_styles": not_included_styles,
+                                       "imports": {"zip_longest": zip_longest},
+                                       "provides": provides})
+    
+    if cookie_adj:
+        resp.set_cookie(key = cookiekey, value = cookieval)
+
+    return resp
+
+@app.get("/summary", response_class=HTMLResponse)
+async def summary(request: Request, cookie_adj: t.Optional[str] = None):
+    if cookie_adj:
+        cookiekey, cookieval = parse_cookie_adj(cookie_adj, request.cookies)
+        request.cookies[cookiekey] = cookieval
+
+    included_styles, not_included_styles = get_included_excluded(request.cookies, "styles")
+    included_classes, _ = get_included_excluded(request.cookies, "classes")
+
+    provides = get_provides(included_classes)
+
+    required_feats = {feat for obj in chain(included_styles, included_classes) if obj.required for feat in obj.required.feat_objs}
+    recommended_feats = {feat for obj in chain(included_styles, included_classes) if obj.recommended for feat in obj.recommended.feat_objs}
+
+    resp = templates.TemplateResponse("summary.html",
+                                      {"request": request,
+                                       "cookies": gen_sidebar(request.cookies),
+                                       "omnimap": omnimap,
+                                       "included_styles": included_styles,
+                                       "not_included_styles": not_included_styles,
+                                       "included_classes": included_classes, 
+                                       "required_feats": required_feats,
+                                       "recommended_feats": recommended_feats,
+                                       "provides": provides})
+    
+    if cookie_adj:
+        resp.set_cookie(key = cookiekey, value = cookieval)
+
+    return resp
+
+
+
+asyncio.run(serve(app, config))
